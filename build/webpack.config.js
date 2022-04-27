fa13eb79f684cacbe5d6e988e5bc4d0ca4e81dd6
荣 <15035291867@163.com>
2022-04-27 22:32:46 +0800

    feat: 抽取jsRules文件

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = require('./paths');
-const {styleRules} = require('./rules/styleLoaders.js');
-
+const {styleRules} = require('./rules/styleRules.js');
+const jsRules = require('./rules/jsRules');
 
 module.exports = {
     mode: process.env.NODE_ENV,
@@ -24,20 +24,7 @@ module.exports = {
         strictExportPresence: true,
         // loaders
         rules: [
-            {
-                test: /\.(js|mjs|jsx|ts|tsx)$/,
-                include: paths.appSrc,
-                use: {
-                    // 当有设置cacheDirectory时，指定的目录将用来缓存 loader 的执行结果。
-                    loader: 'babel-loader?cacheDirectory',
-                    options: {
-                    presets: ['@babel/preset-env', "@babel/preset-react"],
-                    // 'transform-runtime' 插件告诉 Babel,要引用runtime来代替注入
-                    // 引入 @babel/plugin-transform-runtime 并且使所有辅助代码从这里引用。
-                    plugins: ['@babel/plugin-transform-runtime'],
-                    }
-                }
-            },
+            ...jsRules,
             ...styleRules,
         ]
     },
